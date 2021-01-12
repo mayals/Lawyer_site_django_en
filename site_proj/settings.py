2094539cb911298b009c653b3690c46526057285
mayal <mayalsanea2016@gmail.com>
2021-01-12 15:37:19 +0300

    change in requirements.txt & settings.py

--- site_proj/settings.py
+++ site_proj/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     # Simplified static file serving.
+     # https://warehouse.python.org/project/whitenoise/
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -153,7 +155,11 @@ STATICFILES_DIRS = (
 )
 
 
+# https://devcenter.heroku.com/articles/django-assets #
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
